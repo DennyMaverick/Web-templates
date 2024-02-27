280f86f5f7bd14846b0be7194559e4826f82e5ed
Denny Maverick <den_maverick177@mail.ru>
2024-02-27 15:55:21 +0300

    Удаление плагина для gulp gulp-webp-css

--- task/scss.js
+++ task/scss.js
@@ -13,30 +13,35 @@ const rename = require("gulp-rename");
 const size = require("gulp-size");
 const groupMedia = require("gulp-group-css-media-queries");
 const sass = require("gulp-sass")(require('sass'));
+const shorthand = require('gulp-shorthand');
 const sassGlob = require('gulp-sass-glob');
-const webpCss = require('gulp-webp-css');
-
+// const webpCss = require('gulp-webp-css');
 
 // Обработка SCSS
 const scss = () => {
-  return src(path.scss.src, { sourcemaps: app.isDev })
-    .pipe(plumber({
-      errorHandler: notify.onError(error => ({
-        title: 'SCSS',
-        message: error.message
-      }))
-    }))
-    .pipe(sassGlob())
-    .pipe(sass(app.scss))
-    .pipe(webpCss())
-    .pipe(autoprefixer())
-    .pipe(groupMedia())
-    .pipe(size({ title: 'main.css' }))
-    .pipe(dest(path.scss.dest, { sourcemaps: app.isDev }))
-    .pipe(csso())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(size({ title: 'main.min.css' }))
-    .pipe(dest(path.scss.dest, { sourcemaps: app.isDev }));
-}
+  return (
+    src(path.scss.src, {sourcemaps: app.isDev})
+      .pipe(
+        plumber({
+          errorHandler: notify.onError(error => ({
+            title: 'SCSS',
+            message: error.message,
+          })),
+        })
+      )
+      .pipe(sassGlob())
+      .pipe(sass(app.scss))
+      // .pipe(webpCss())
+      .pipe(autoprefixer())
+      .pipe(shorthand())
+      .pipe(groupMedia())
+      .pipe(size({title: 'main.css'}))
+      .pipe(dest(path.scss.dest, {sourcemaps: app.isDev}))
+      .pipe(csso())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(size({title: 'main.min.css'}))
+      .pipe(dest(path.scss.dest, {sourcemaps: app.isDev}))
+  );
+};
 
 module.exports = scss;
